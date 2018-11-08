1a2dc357e55e571af98f24df1730227b23ffd0c0
RaidenRabit <1062021@ucn.dk>
2018-11-08 18:07:15 +0100

    #8 created test and core projects; refactored existing code

---
+++ MAP/Tests/Integration tests/IntegrationFakeServer.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class IntegrationFakeServer
+    {
+        private HttpServer _server;
+        protected HttpClient _client;
+
+        [SetUp]
+        protected void StartServer()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            MAPeApi.WebApiConfig.Register(config);
+            _server = new HttpServer(config);
+            _client = new HttpClient(_server);
+        }
+
+        [TearDown]
+        protected void Dispose()
+        {
+            _server.Dispose();
+            _client.Dispose();
+        }
+    }
+}
